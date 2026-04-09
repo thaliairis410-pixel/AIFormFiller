c095c2bf9739bda319905b7798cd7dab23d85e37
Victor Onah <victoronah1914@gmail.com>
2026-04-09 02:30:58 -0700

    chore: increase worker threads number

--- src/server/services/queue.service.ts
+++ src/server/services/queue.service.ts
@@ -16,7 +16,7 @@ export class QueuingService {
     // Seed the queue before spawning workers
     await QueuingService.refill();
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 10; i++) {
       const workerPath = import.meta.resolve("../utils/worker.util.js");
       const worker = new Worker(
         `import('tsx/esm/api').then(({ register }) => { register(); import('${workerPath}'); })`,
